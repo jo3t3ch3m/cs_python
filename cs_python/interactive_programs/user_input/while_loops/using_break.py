15ef33e35c734d4c3ad56244077eb3baaf181bf2
jo3t3ch3m <joe@techemstudios.com>
2017-02-20 17:34:56 -0500

    break the loop feature

--- cs_python/interactive_programs/user_input/while_loops/using_break.py
+++ cs_python/interactive_programs/user_input/while_loops/using_break.py
@@ -1,51 +1,30 @@
 # Python 2.7
 
+# Using break to Exit a loop
 
-# again, consider a imple parrot program...
-##prompt = ("\nTell me something, and I will repeat it back to you:")
-##prompt += "\nEnter 'quit' to end the program. "
-##
-##message = ""
-##while message != 'quit':
-##    message = raw_input(prompt)
+# So far, we've looked at stopping a while loop by using a flag variable
+# You can also use a BREAK statement...
+# To exit a while loop immediately without running any remaining code in the loop,
+# regardless of the results of any conditional test, use the break statement.
 
-##    if message != 'quit':
-##        print(message)
+# The break statement directs the flow of your program
+# You can use it to control which lines of code are executed and which aren't,
+# so the program only executes code that you want it to, wehn you want it to
+# Think of the term abstraction?
 
-# This program performs its task while a given condition is true.
-# But, what about a more compplicated programs in which many different events
-# could cause the program to stop running?
+# Let's look at an example
+# Consider a program that asks the user about places they've visited.
+# To stop the while loop in this program by calling break as soon
+# as the user enters the 'quit' value:
 
-# Think of a game.
-# Usually, several differentt events can end the game.
-# i.e. when time runs out, when the player dies, when an enemy reaches a certain point, etc.
-# If these examples occur, it can make the program end.
+prompt = ("\nPlease enter the name of a city you have visited:")
+prompt += "\n(Enter 'quit' when you are finished.) "
 
-# If many possible events might occur to stop the program, just trying to test
-# all these conditions in one while statement can become complicated and difficult!
-###################
-# Enter... FLAG
-# For a program that should run only as long as many conditions are true, you
-# can define one variable that determiens whether or not the entire the program is active.
-# This variable, called a flag, acts as a signal to the program.
+while True:
+    city = raw_input(prompt)
 
-# We can run our programs so they run while the flag is set to True
-# and stop running when any of several events sets the value of the flag to False
-
-# so... SO, our overall while statement needs to check only one condition:
-# whether or not the flag is currently True.
-# This way, all our other tests can be neatly organized in the rest of the program.
-
-# Alright, enough talking, let's add a flag to the parrot program!!!
-
-prompt = ("\nTell me something, and I will repeat it back to you:")
-prompt += "\nEnter 'quit' to end the program. "
-
-active = True # starting our flag
-while active:
-    message = raw_input(prompt)
-
-    if message != 'quit':
-        active = False # checking flag validity
+    if city == 'quit':
+        break
     else:
-        print(message)
+        print("I'd love to go " + city.title() + "!")
+
