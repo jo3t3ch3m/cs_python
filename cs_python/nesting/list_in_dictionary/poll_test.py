b3ef4e68f427c57ab788103a1d5fd20f95f4a0ae
jo3t3ch3m <joe@techemstudios.com>
2017-02-22 12:41:09 -0500

    added city nesting file and fixed attr. error

--- cs_python/nesting/list_in_dictionary/poll_test.py
+++ cs_python/nesting/list_in_dictionary/poll_test.py
@@ -13,5 +13,5 @@ favorite_languages = {
 for name, languages in favorite_languages.items():
     print("\n" + name.title() + "'s favorite languages are:")
     for language in languages:
-        print("\t" + language.title())
+        print("\t" + language.title()) # fixed attribute error ("languages" to language")
         
