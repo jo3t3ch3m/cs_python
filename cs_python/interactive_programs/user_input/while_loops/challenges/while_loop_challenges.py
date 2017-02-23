0a2784fa75742968ef1c13e33042db35ec936bf7
joetechem <joe@techemstudios.com>
2017-02-23 16:33:54 -0500

    while loop class challenges

--- cs_python/interactive_programs/user_input/while_loops/challenges/while_loop_challenges.py
+++ cs_python/interactive_programs/user_input/while_loops/challenges/while_loop_challenges.py
@@ -2,3 +2,12 @@
 # Write a loop that prompts the user to enter a series of pizza toppings
 # until they enter a 'quit' value. As they enter each topping, print a message
 # saying you'll add that topping to their pizza.
+
+# Movie Tickets:
+# Let's say a zoo charges different ticket prices depending on
+# a person's age. If a person is under the age of 5, the ticket is free;
+# if they are between 5 and 12, the ticket is $10;
+# and if they are over age 12, the ticket is $15.
+# Write a loop where you ask users their age, and then tell them the cost
+# of the zoo ticket.
+
