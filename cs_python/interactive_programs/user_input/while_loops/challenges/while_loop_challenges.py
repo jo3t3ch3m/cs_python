af4c8a134a367636fdf08b08e0457cffc974a1e1
joetechem <joe@techemstudios.com>
2017-02-23 16:39:10 -0500

    adding challenges

--- cs_python/interactive_programs/user_input/while_loops/challenges/while_loop_challenges.py
+++ cs_python/interactive_programs/user_input/while_loops/challenges/while_loop_challenges.py
@@ -11,3 +11,14 @@
 # Write a loop where you ask users their age, and then tell them the cost
 # of the zoo ticket.
 
+# Practicing the Three Exits
+# We've learned three techniques to end a loop:
+# 1) conditional test to stop the loop
+# 2) use an active variable to control how long the loop runs
+# 3) use a break statement to exit the loop when the user enters a value (like 'quit')
+
+# The challenge is to write three programs for each technique.
+
+# Neverending Loop:
+# Write a loop that never ends, and run it!
+# Remember to press CTRL C to exit.
