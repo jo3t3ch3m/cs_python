a6fe146b3e8c044b419e05ad10341512444b9d37
joetechem <joe@techemstudios.com>
2017-03-09 13:29:05 -0500

    starting classes

---
+++ cs_python/oop/classes_intro.py
@@ -0,0 +1,32 @@
+class Dog():
+    """a simple attempt to model a dog"""
+
+    def __init__(self, name, age):
+        """Initializa name and age attributes."""
+        self.name = name
+        self.age = age
+
+    def sit(self):
+        """Simulate a dog sitting in response to a command."""
+        print(self.name.title() + " is now sitting.")
+
+    def roll_over(self):
+        """Simulate rolling over in response to a command."""
+        print(self.name.title() + " rolled over!")
+
+
+# Making an instance representing a specific dog
+
+my_dog = Dog('koa', 2)
+
+print("My dog's name is " + my_dog.name.title() + ".")
+print("My dog is " +str(my_dog.age) + " years old.")
+
+# CALLING METHODS
+my_dog.sit()
+my_dog.roll_over()
+
+# CREATING MULTIPLE INSTANCES
+
+your_dog('teddy', 3)
+
