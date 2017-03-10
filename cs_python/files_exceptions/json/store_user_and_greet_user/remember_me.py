1a9a4ceedb516a6d1670454cf366d82025709f38
joetechem <joe@techemstudios.com>
2017-03-10 15:02:02 -0500

    refactoring

--- cs_python/files_exceptions/json/store_user_and_greet_user/remember_me.py
+++ cs_python/files_exceptions/json/store_user_and_greet_user/remember_me.py
@@ -21,24 +21,57 @@
 # Editing the code to combine remember_me and greet_user in one file.
 # So, the original code above will be commented, so we can still look to what we did earlier
 
-import json
-
-# Load the username, if it has been stored previously.
-#       Otherwise, ask the user for a name and store it
-filename = 'username.json'
-try:
-    with open(filename) as f_obj:
-        username = json.load(f_obj)
-
-except IOError:
-    username = raw_input("What is your name? ")
-    with open(filename, 'w') as f_obj:
-        json.dump(username, f_obj)
-        print("We'll keep your name stored for when you come back, " + username.title() + "!")
-else:
-    print("\nWelcome back, " + username + "!")
+##import json
+##
+### Load the username, if it has been stored previously.
+###       Otherwise, ask the user for a name and store it
+##filename = 'username.json'
+##try:
+##    with open(filename) as f_obj:
+##        username = json.load(f_obj)
+##
+##except IOError:
+##    username = raw_input("What is your name? ")
+##    with open(filename, 'w') as f_obj:
+##        json.dump(username, f_obj)
+##        print("We'll keep your name stored for when you come back, " + username.title() + "!")
+##else:
+##    print("\nWelcome back, " + username + "!")
 
 # The above code worked, except when you delete the JSON file,"FileNotFoundError is not defined"
 # Python 2.7 uses "IOError" rather than "FileNotFoundError"
 
-        
+# So the code works now. But, we've seen before when writing our programs,
+# we might recognize that we can make our code better by breaking it up into
+# a series of fucntions that perform specific jobs.
+# This is called REFACTORING. This makes your code cleaner, easier to understand,
+# and easier to expand.
+
+# Let's Refactor the above the code to do just that
+
+# REFACTORING
+
+###
+###
+
+import json
+
+def greet_user():
+    """Greet the user by their name."""
+    filename = 'username.json'
+    try:
+        with open(filename) as f_obj:
+            username = json.load(f_obj)
+
+    except IOError:
+        username = raw_input("What is your name? ")
+        with open(filename, 'w') as f_obj:
+            json.dump(username, f_obj)
+            print("We'll keep your name stored for when you come back, " + username.title() + "!")
+    else:
+        print("\nWelcome back, " + username + "!")
+
+greet_user()
+
+# Because we are using a function now, we updated the comments with a docstring
+# that shows how the program currently works
