3a496c453b81b2e906248c0e2a5cafc2b0d7f8a7
joetechem <joe@techemstudios.com>
2017-03-13 17:02:55 -0400

    object-oriented outline/notes

---
+++ cs_python/oop/detail_intro.py
@@ -0,0 +1,42 @@
+# Python 2.7
+
+# OBJECT-ORIENTED PROGRAMMING
+
+# One of the most effective approaches to wriitng software.
+
+# Object oriented programming is amazing at modeling real-world situations
+
+# In Object-oriented programming, you write classes that represent real-world
+# things and situations, and you create objects based on these classes.
+# In other words...
+# Objects are defined by Classes
+
+# When you you writea class, you define the general behavior that
+# a whole category of objects can have.
+
+# So, when you create individual objects from the class, each bject is
+# automatically equipped with he general behavior; you can then give each
+# object whatever unique traits you desire.
+
+# When we make an object from a class, it is called INSTANTIATION,
+# and we work with instances of a class.
+
+# Objectives:
+# We'll learn how to write classes and create instances of those classes.
+# We'll specify the kind of information that can be stored in instances,
+# and we'll define actions that can be taken with these isntances.
+
+# We'll write classes that extend the functionality of existing classes,
+# so similar classes can share code efficiently.
+
+# Our classes we make, will be stored in modules and we'll import classes that
+# have been written by other programmers in our own program files.
+
+# Once we've dived in object-oriented programming, we'll really understand
+# the code we write, not only will we understand what's happening line by line,
+# but also the bigger concepts behind them.
+
+# Object-oriented programming will train us to think logically so we will be able
+# to effectively approach almost any problem.
+
+# move on to class_intro.py
