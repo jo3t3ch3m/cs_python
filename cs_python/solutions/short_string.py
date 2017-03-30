a72be210fb075f8f0fd116e162a3a2fe4b457ec6
joe <joe@techemstudios.com>
2017-03-30 14:33:33 -0400

    finishing

--- cs_python/solutions/short_string.py
+++ cs_python/solutions/short_string.py
@@ -1,1 +1,11 @@
-aa
+name = "ada lovelace"
+print(name.title())
+
+first_name = "ada"
+last_name = "lovelace"
+full_name = first_name + " " + last_name
+
+print(full_name)
+
+message = "Hello " + full_name.title() + "!"
+print(message)
