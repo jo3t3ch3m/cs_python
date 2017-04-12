fd6654779f1f046f3476e485b6a8c5b312088631
joetechem <joe@techemstudios.com>
2017-04-12 16:03:25 -0400

    setting up title and labels for the axes

--- cs_python/data_visualization/generating_data/scatter/scatter_squares.py
+++ cs_python/data_visualization/generating_data/scatter/scatter_squares.py
@@ -17,7 +17,15 @@ values of the point of interest to scatter(), and it should plot those values
 
 import matplotlib.pyplot as plt
 
-plt.scatter(2, 4)
+plt.scatter(2, 4, s = 200)
+
+# Set chart title and label axes.
+plt.title("Square Numbers", fontsize = 24)      # setting up title
+plt.xlabel("Value", fontsize = 14)              # setting up x axis label
+plt.ylabel("Square of Value", fontsize = 14)    # setting up y axis label
+
+# Set size of tick labels.
+plt.tick_params(axis = 'both', which = 'major, labelsize = 14)
 
 plt.show()
 
