ab4d0653e9ab5b09a70547962bfc6117f540b7aa
joetechem <joe@techemstudios.com>
2017-04-12 16:25:52 -0400

    passing an RGB model

--- cs_python/data_visualization/generating_data/scatter/d_scatter_series.py
+++ cs_python/data_visualization/generating_data/scatter/d_scatter_series.py
@@ -13,8 +13,9 @@ import matplotlib.pyplot as plt
 x_values = list(range(1, 1001))
 y_values = [x**2 for x in x_values]
 
-# change the color of the points:
-plt.scatter(x_values, y_values, c= 'red', edgecolor = 'none', s = 40)
+# change the color of the points
+# with an RGB model:
+plt.scatter(x_values, y_values, c = (0.2, 0.5, 0.1), edgecolor = 'none', s = 40)
 
 
 plt.title("Square Numbers", fontsize = 24)      # setting up title
