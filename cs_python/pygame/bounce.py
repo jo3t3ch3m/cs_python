4bf43f275572272150cc2f793e5cf8802f845b42
joe <joe@techemstudios.com>
2017-04-20 12:13:41 -0400

    testing with pygame

---
+++ cs_python/pygame/bounce.py
@@ -0,0 +1,27 @@
+import sys, pygame
+pygame.init()
+
+size = width, height = 600, 600
+speed = [1, 1]
+red = 130, 202, 154
+
+screen = pygame.display.set_mode(size)
+
+
+# ball = pygame.image.load("pygame_logo.gif")
+ball = pygame.image.load("TechEmDogKoa1.jpg")
+ballrect = ball.get_rect()
+
+while 1:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: sys.exit()
+
+    ballrect = ballrect.move(speed)
+    if ballrect.left < 0 or ballrect.right > width:
+        speed[0] = -speed[0]
+    if ballrect.top < 0 or ballrect.bottom > height:
+        speed[1] = -speed[1]
+
+    screen.fill(red)
+    screen.blit(ball, ballrect)
+    pygame.display.flip()
